804fa64839eaa6f91ca24ee3e81741ded7914ed1
Fernando Cruz <f.cruzval@gmail.com>
2017-10-04 18:39:47 +0000

    Created seeds file for sample data

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,14 @@ module DevCampPortfolio
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    
+    # myself
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+      g.test_framework  :test_unit, fixture: false
+      g.stylesheets     false
+      g.javascripts     false
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |blog|
+  Blog.create!(
+    title: "My blog post #{blog + 1}",
+    body: "lorem  ipsum odoñn aosn añsnaños nañonsoauewou oeuwoeuq spnasnpconp"
+  )
+end
+puts "10 Blog posts created."
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill + 1}",
+    percent_utilized: 100
+  )
+end
+puts "5 Skills created."
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio #{portfolio_item + 1}",
+    subtitle: "My great service",
+    body: "Lorem ipsum aci venidi comidi vididi",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+puts "9 portfolio items created."
